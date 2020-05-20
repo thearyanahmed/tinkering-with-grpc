498a08949f415fed0ba24b6fdd0e6bcf6f0fb8f9
theprophecy <thearyanahmed@gmail.com>
2020-05-20 21:17:46 +0600

    added grpc server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/thearyanahmed/tinkering-with-grpc
 
 go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.0
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.23.0
+)
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 
+
 	"google.golang.org/grpc"
-	"grpc-udemy-course/greet/greetpb"
+	"github.com/thearyanahmed/tinkering-with-grpc/greet/greetpb"
 )
 
 type server struct{}
@@ -13,13 +15,19 @@ type server struct{}
 func main() {
 	fmt.Println("Starting server")
 
-	// listener, err := net.Listen("tcp","0.0.0.0:50051")
+	listener, err := net.Listen("tcp","0.0.0.0:50051")
 
-	// if err != nil {
-	// 	log.Fatal("Error: %v",err)
-	// }
+	if err != nil {
+		log.Fatal("Error: %v",err)
+	}
 
-	// s := grpc.NewServer()
+	s := grpc.NewServer()
 
-	// greetpb.Re
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	err = s.Serve(listener)
+
+	if err != nil {
+		log.Fatal("Error Serving greetpb. %v",err)
+	}
 }
